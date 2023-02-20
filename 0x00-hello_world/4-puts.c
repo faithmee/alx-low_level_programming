692d4f242a612e7349b287038ecbcf04f7c3b54d
faithmee <faithnderitu3438@gmail.com>
2023-02-20 06:28:55 -0800

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program printing a line with puts function
- * Return:0 
- */ 
+ *main - entry point 
+ *Return: always 0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
