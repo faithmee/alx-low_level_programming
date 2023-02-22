20725675efe767628e03498fd3e55dffe698f185
faithmee <faithnderitu3438@gmail.com>
2023-02-22 00:03:38 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - Prints the sign of the number
+*
+*Return : Always 0 (success)
+*/
+
+int main(void)
+{
+int print_sign(void);
+int n;
+	if (n > 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(0);
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
