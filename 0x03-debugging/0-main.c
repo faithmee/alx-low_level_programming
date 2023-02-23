3e88b54baa6ec8dc6ebebdcb7e0a63b9a49891b7
faithmee <faithnderitu3438@gmail.com>
2023-02-23 06:47:11 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  *main - Test function for positive or negative
+ *description: positive or negative i
  *Return: 0
  */
 int main(void)
@@ -8,7 +9,7 @@ int main(void)
 	int i;
 
 	i = 0;
-	positive_or_negative(1);
+	positive_or_negative(i);
 
 	return (0);
 }
