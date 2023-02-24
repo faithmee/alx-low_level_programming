6082888ef438a8d9f4e8cb844a20213ebc8b90c7
faithmee <faithnderitu3438@gmail.com>
2023-02-23 22:58:42 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,23 @@
 #include "main.h"
-/**
+
+/*
+ *main - Check the code
+ *description: Write a function that checks for a digit (0 through 9).
+ *
  *_isdigit - Check if numbers are 0 - 9
  *@x: Char to b checked
+ *
  *Return: 0 or 1
  */
-int _isdigit(int x);
+
+int main(void)
 {
+	int _isdigit(int x);
+	{
 	if (x >= '48' && x <= '57')
 	{
 		return (1);
 	}
 	return (0);
+	}
 }
