d8580d923d5150de1cfadeb0b26c9c40711e5e58
faithmee <faithnderitu3438@gmail.com>
2023-02-24 11:35:52 -0800

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,15 @@
 #include "main.h"
+
 /**
- *main - Check the code
- *description: Write a function that checks for a digit (0 through 9)
  *_isdigit - Check if numbers are 0 - 9
+ *@x: Char to be checked
  *Return: 0 or 1
  */
-int main(void)
+
+int _isdigit(int x);
 {
-	int _isdigit(int x);
-	{
-	if (x >= '48' && x <= '57')
-	{
+	if (x >= '0' && x <= '9')
 		return (1);
-	}
-	return (0);
-	}
+	else
+		return (0);
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *main - Check the code
+ *description - Write a function that prints the numbers and by a new line
+ *print_most_numbers - Print num except 2 and 4
+ *@c - character
+ *description - print_most_numbers
+ *Return: void
+ */
+int main(void)
+{
+	void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
+}
