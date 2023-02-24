1b2689b222e501c7a65a40561b542e0f9b214716
faithmee <faithnderitu3438@gmail.com>
2023-02-24 14:03:08 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -24,7 +24,7 @@ int main(void)
 			else if (i % 3 == 0 && i % 5 == 0)
 			{
 				printf("FizzBuzz");
-				}
+			}
 			else if (i == 1)
 			{
 				printf("%d", i);
@@ -34,7 +34,8 @@ int main(void)
 				printf("%d", i);
 			}
 		}
-	printf("\n");
+		printf("\n");
 
-	return (0);
+		return (0);
 }
+
