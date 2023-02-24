479738dc165985a725d3aae0976fcbafed33b8a7
faithmee <faithnderitu3438@gmail.com>
2023-02-24 15:21:07 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,9 @@
  *mul - multiply 2 int
  *@a: first int
  *@b: second int
-*Return: addition of the multiplication
-*/
-
-int mul(int a, int b);
-(
+ *Return: addition of the multiplication
+ */
+int mul(int a, int b)
 {
-	return (a + b);
-	);
+	return (a * b);
 }
